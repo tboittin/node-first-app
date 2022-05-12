64680f6caab9bb464b66ca426816537bb0a0aba2
Thomas Boittin <tboittin@gmail.com>
2022-05-12 11:37:07 +0200

    Adding Controllers

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', 'views')
 
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded(
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded(
     ))
 app.use(express.static(path.join(__dirname,'public')))
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
