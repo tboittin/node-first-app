34f9b251e3ae4c40a997f9e853ca4ff385bcbc4b
Thomas Boittin <tboittin@gmail.com>
2022-05-12 15:02:40 +0200

    Finishing the controllers

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const path = require('path')
+const path = require("path")
 const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
+app.set("view engine", "pug")
+app.set("views", "views")
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+const errorController = require("./controllers/404")
 
-app.use(bodyParser.urlencoded(
-    // {extended: false}
-    ))
-app.use(express.static(path.join(__dirname,'public')))
+app.use(
+    bodyParser
+        .urlencoded
+        // {extended: false}
+        ()
+)
+app.use(express.static(path.join(__dirname, "public")))
 
-app.use('/admin', adminRoutes)
+app.use("/admin", adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'})
-})
+app.use(errorController.get404)
 
 app.listen(3000)
