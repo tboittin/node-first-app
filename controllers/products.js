ad77401bc33dbdad09e1ba8d5176cb05aee9351c
Thomas Boittin <tboittin@gmail.com>
2022-05-13 10:50:53 +0200

    Creating the Shop Structure

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require("../models/product")
 exports.getAddProduct = (req, res, next) => {
-    res.render("add-product", {
+    res.render("./admin/add-product", {
         pageTitle: "Add Product",
         path: "/admin/add-product",
     })
@@ -14,6 +14,6 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-        res.render("shop", { prods: products, pageTitle: "Shop", path: "/" })
+        res.render("./shop/product-list", { prods: products, pageTitle: "Shop", path: "/" })
     })
 }
